9370d19cc3328cc9d21115dd8b633478cdc42843
Denisseglez200 <gobd2225i@gmail.com>
2025-06-11 10:55:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 window.onload = function() {
-var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "")
-alert("Es un gusto tenerte aquí " + reply + "!")
+var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "");
+alert("Es un gusto tenerte aquí " + reply + "!");
 }
 let miBoton = document.querySelector('button');
 miBoton.onclick = function() {
